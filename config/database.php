8eeddedfb68c99e19aac04b455b388fe0855589e
Ileenfdz <ileenfdz@gmail.com>
2022-04-15 22:20:24 +0200

    Added some mini changes

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'pokemon-mini-web'),
+            'database' => env('DB_DATABASE', 'pokemon_mini_web'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
